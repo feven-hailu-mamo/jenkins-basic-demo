11443170eca5abff9ceb4f23ca87313674f3f1c5
Feven <feven.h.mamo@gmail.com>
2020-05-04 15:01:15 -0500

    java file updated

--- TestProjectJenkins.java
+++ TestProjectJenkins.java
@@ -3,5 +3,6 @@ public class TestProjectJenkins{
 public static void main(String[] args){
   for(int i=1; i<=10; i++)
   System.out.println("Hello World" + i);
+System.out.println("Some changes made");
 }
 }
